bb938030db584172ebfb53b075a0de7326e97784
matheusgondra <matheusgondra951@gmail.com>
2026-09-08 07:40:28 -0300

    feat: create cpu.h

---
+++ include/cpu.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef struct _chip8 {
+    uint8_t memory[4 * 1024];
+    uint8_t V[16];
+    uint16_t I;
+    uint16_t pc;
+    uint8_t sp;
+    uint16_t stack[16];
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+    bool keyboard[16];
+    uint8_t diaplay[32][64];
+    bool draw_flag;
+} Chip8;
+
+bool chip8_init(Chip8 *cpu);
+bool chip8_load_rom(Chip8 *cpu, const char *filename);
+void chip8_cycle(Chip8 *cpu);

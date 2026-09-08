7053f597ed629d043267d2bfe7b44af425af232d
matheusgondra <matheusgondra951@gmail.com>
2026-09-08 15:07:39 -0300

    feat: create Opcode struct

--- include/cpu.h
+++ include/cpu.h
@@ -14,10 +14,76 @@ typedef struct _chip8 {
     uint8_t delay_timer;
     uint8_t sound_timer;
     bool keyboard[16];
-    bool diaplay[32][64];
+    bool display[32][64];
     bool draw_flag;
 } Chip8;
+
+typedef enum {
+    // System and Flow
+    OP_SYS_0NNN,      // 0NNN - Native machine call (ignored/NOP)
+    OP_CLS_00E0,      // 00E0 - Clear screen
+    OP_RET_00EE,      // 00EE - Return from subroutine
+    OP_JP_1NNN,       // 1NNN - Unconditional jump
+    OP_CALL_2NNN,     // 2NNN - Subroutine call
+
+    // Comparisons / Conditionals
+    OP_SE_3XNN,       // 3XNN - Skip next instruction if Vx == NN
+    OP_SNE_4XNN,      // 4XNN - Skip next instruction if Vx != NN
+    OP_SE_5XY0,       // 5XY0 - Skip next instruction if Vx == Vy
+    OP_SNE_9XY0,      // 9XY0 - Skip next instruction if Vx != Vy
+
+    // Assignment and Basic Arithmetic
+    OP_LD_6XNN,       // 6XNN - Vx = NN
+    OP_ADD_7XNN,      // 7XNN - Vx += NN
+
+    // Logical and Mathematical Operations (8 family)
+    OP_LD_8XY0,       // 8XY0 - Vx = Vy
+    OP_OR_8XY1,       // 8XY1 - Vx |= Vy
+    OP_AND_8XY2,      // 8XY2 - Vx &= Vy
+    OP_XOR_8XY3,      // 8XY3 - Vx ^= Vy
+    OP_ADD_8XY4,      // 8XY4 - Vx += Vy (VF = carry)
+    OP_SUB_8XY5,      // 8XY5 - Vx -= Vy (VF = NOT borrow)
+    OP_SHR_8XY6,      // 8XY6 - Vx >>= 1
+    OP_SUBN_8XY7,     // 8XY7 - Vx = Vy - Vx (VF = NOT borrow)
+    OP_SHL_8XYE,      // 8XYE - Vx <<= 1
+
+    // Memory, Special Jumps, and Graphics
+    OP_LD_I_ANNN,     // ANNN - I = NNN
+    OP_JP_V0_BNNN,    // BNNN - Jump to NNN + V0
+    OP_RND_CXNN,      // CXNN - Vx = rand() & NN
+    OP_DRW_DXYN,      // DXYN - Draw sprite of N rows
+
+    // Keyboard
+    OP_SKP_EX9E,      // EX9E - Skip if key Vx is pressed
+    OP_SKNP_EXA1,     // EXA1 - Skip if key Vx is NOT pressed
+
+    // Timers and Miscellaneous (F family)
+    OP_LD_VX_DT_FX07, // FX07 - Vx = delay_timer
+    OP_WAIT_KEY_FX0A, // FX0A - Wait for a key press and save it in Vx
+    OP_SET_DT_FX15,   // FX15 - delay_timer = Vx
+    OP_SET_ST_FX18,   // FX18 - sound_timer = Vx
+    OP_ADD_I_FX1E,    // FX1E - I += Vx
+    OP_LD_FONT_FX29,  // FX29 - I = font sprite for the digit in Vx
+    OP_BCD_FX33,      // FX33 - Store BCD of Vx in I, I+1, I+2
+    OP_DUMP_REGS_FX55,// FX55 - Store V0..Vx in RAM starting at I
+    OP_LOAD_REGS_FX65,// FX65 - Load RAM starting at I into V0..Vx
+
+    OP_INVALID        // Opcode not recognized
+} OpcodeType;
+
+typedef struct _opcode {
+    uint16_t raw;
+    uint8_t category;
+    uint8_t x;
+    uint8_t y;
+    uint8_t n;
+    uint8_t nn;
+    uint16_t nnn;
+    OpcodeType type;
+} Opcode;
 
 bool chip8_init(Chip8 *cpu);
 bool chip8_load_rom(Chip8 *cpu, const char *filename);
 void chip8_cycle(Chip8 *cpu);
+
+Opcode decode_opcode(uint16_t raw_opcode);
--- src/cpu.c
+++ src/cpu.c
@@ -1,4 +1,5 @@
 #include "cpu.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -36,4 +37,112 @@ bool chip8_load_rom(Chip8 *cpu, const char *filename) {
 
     fclose(file);
     return true;
+}
+
+static uint16_t fetch_opcode(const Chip8 *cpu) {
+    uint16_t opcode = (cpu->memory[cpu->pc] << 8) | cpu->memory[cpu->pc + 1];
+    return opcode;
+}
+
+static OpcodeType resolve_opcode_type(uint16_t raw) {
+    uint8_t category = (raw & 0xF000) >> 12;
+    uint8_t n        = raw & 0x000F;
+    uint8_t nn       = raw & 0x00FF;
+
+    switch (category) {
+        case 0x0:
+            if (nn == 0xE0) {
+                return OP_CLS_00E0;
+            }
+
+            if (nn == 0xEE) {
+                return OP_RET_00EE;
+            }
+            
+            return OP_SYS_0NNN;
+
+        case 0x1: return OP_JP_1NNN;
+        case 0x2: return OP_CALL_2NNN;
+        case 0x3: return OP_SE_3XNN;
+        case 0x4: return OP_SNE_4XNN;
+        case 0x5: return (n == 0x0) ? OP_SE_5XY0 : OP_INVALID;
+        case 0x6: return OP_LD_6XNN;
+        case 0x7: return OP_ADD_7XNN;
+
+        case 0x8:
+            switch (n) {
+                case 0x0: return OP_LD_8XY0;
+                case 0x1: return OP_OR_8XY1;
+                case 0x2: return OP_AND_8XY2;
+                case 0x3: return OP_XOR_8XY3;
+                case 0x4: return OP_ADD_8XY4;
+                case 0x5: return OP_SUB_8XY5;
+                case 0x6: return OP_SHR_8XY6;
+                case 0x7: return OP_SUBN_8XY7;
+                case 0xE: return OP_SHL_8XYE;
+                default:  return OP_INVALID;
+            }
+
+        case 0x9: return (n == 0x0) ? OP_SNE_9XY0 : OP_INVALID;
+        case 0xA: return OP_LD_I_ANNN;
+        case 0xB: return OP_JP_V0_BNNN;
+        case 0xC: return OP_RND_CXNN;
+        case 0xD: return OP_DRW_DXYN;
+
+        case 0xE:
+            if (nn == 0x9E) {
+                return OP_SKP_EX9E;
+            }
+
+            if (nn == 0xA1) {
+                return OP_SKNP_EXA1;
+            }
+
+            return OP_INVALID;
+
+        case 0xF:
+            switch (nn) {
+                case 0x07: return OP_LD_VX_DT_FX07;
+                case 0x0A: return OP_WAIT_KEY_FX0A;
+                case 0x15: return OP_SET_DT_FX15;
+                case 0x18: return OP_SET_ST_FX18;
+                case 0x1E: return OP_ADD_I_FX1E;
+                case 0x29: return OP_LD_FONT_FX29;
+                case 0x33: return OP_BCD_FX33;
+                case 0x55: return OP_DUMP_REGS_FX55;
+                case 0x65: return OP_LOAD_REGS_FX65;
+                default:   return OP_INVALID;
+            }
+
+        default:
+            return OP_INVALID;
+    }
+}
+
+Opcode decode_opcode(uint16_t raw_opcode) {
+    return (Opcode) {
+        .raw = raw_opcode,
+        .category = (raw_opcode & 0xF000) >> 12,
+        .x = (raw_opcode & 0x0F00) >> 8,
+        .y = (raw_opcode & 0x00F0) >> 4,
+        .n = raw_opcode & 0x000F,
+        .nn = raw_opcode & 0x00FF,
+        .nnn = raw_opcode & 0x0FFF,
+        .type = resolve_opcode_type(raw_opcode)
+    };
+}
+
+void chip8_cycle(Chip8 *cpu) {
+    uint16_t raw_opcode = fetch_opcode(cpu);
+    cpu->pc += 2;
+
+    Opcode opcode = decode_opcode(raw_opcode);
+    switch (opcode.type) {
+        case OP_INVALID:
+            fprintf(stderr, "Invalid opcode: 0x%04X\n", raw_opcode);
+            break;
+        default:
+            fprintf(stdout, "Opcode unknown: 0x%04X\n", raw_opcode);
+            break;
+    }
 }
